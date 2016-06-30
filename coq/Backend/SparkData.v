94621951fa1b298adbf52706da70bdbb8ff6d7f2
Stefan Fehrenbach <sfehren@us.ibm.com>
2016-06-30 10:13:33 -0400

    One more case in data to sparkdata conversion succeeds proof

--- coq/Backend/SparkData.v
+++ coq/Backend/SparkData.v
@@ -45,23 +45,23 @@ Section SparkData.
   Context {m:brand_model}.
 
   Inductive stype :=
-  | stnull   : stype
-  | stint    : stype
-  | ststring : stype
-  | stbool   : stype
-  | starray  : stype -> stype
-  | ststruct : list (string * stype) -> stype
+  | STnull   : stype
+  | STint    : stype
+  | STstring : stype
+  | STbool   : stype
+  | STarray  : stype -> stype
+  | STstruct : list (string * stype) -> stype
   .
 
   Inductive sdata :=
   (* This is outside the actual Spark data model, like .. in open records. Probably use a string representation? *)
-  | sblob   : data -> sdata
-  | snull   : sdata
-  | sint    : Z -> sdata
-  | sstring : string -> sdata
-  | sbool   : bool -> sdata
-  | sarray  : list sdata -> sdata
-  | srow    : list (string * sdata) -> option data -> sdata
+  | Sblob   : data -> sdata
+  | Snull   : sdata
+  | Sint    : Z -> sdata
+  | Sstring : string -> sdata
+  | Sbool   : bool -> sdata
+  | Sarray  : list sdata -> sdata
+  | Srow    : list (string * sdata) -> option data -> sdata
   .
 
   Definition brand_content_rtype (bl: list string) : rtype₀ :=
@@ -70,28 +70,28 @@ Section SparkData.
 
   Definition brand_content_stype (bl: list string) : stype :=
     (* TODO Intersection of brands models, or something *)
-    stnull.
+    STnull.
 
   Fixpoint rtype_to_stype (r: rtype₀) : stype :=
     match r with
-    | Bottom₀ => stnull
-    | Top₀ => ststring
-    | Unit₀ => stnull
-    | Nat₀ => stint
-    | Bool₀ => stbool
-    | String₀ => ststring
-    | Coll₀ e_type => starray (rtype_to_stype e_type)
+    | Bottom₀ => STnull
+    | Top₀ => STstring
+    | Unit₀ => STnull
+    | Nat₀ => STint
+    | Bool₀ => STbool
+    | String₀ => STstring
+    | Coll₀ e_type => STarray (rtype_to_stype e_type)
     | Rec₀ _ fields =>
       let known_fields : list (string * stype) :=
           map (fun p => (fst p, rtype_to_stype (snd p))) fields in
-      ststruct (("$blob"%string, ststring)::("$known"%string, ststruct known_fields)::nil)
-    | Either₀ l r => ststruct (("$left"%string, rtype_to_stype l)::("$right"%string, rtype_to_stype r)::nil)
+      STstruct (("$blob"%string, STstring)::("$known"%string, STstruct known_fields)::nil)
+    | Either₀ l r => STstruct (("$left"%string, rtype_to_stype l)::("$right"%string, rtype_to_stype r)::nil)
     | Brand₀ brands =>
-      ststruct (("$type"%string, starray ststring)
+      STstruct (("$type"%string, STarray STstring)
                   ::("$data"%string, brand_content_stype brands)::nil)
     (* should not occur *)
-    | Arrow₀ _ _ => stnull
-    | Foreign₀ ft => stnull
+    | Arrow₀ _ _ => STnull
+    | Foreign₀ ft => STnull
     end.
 
   Definition flip {a b c} (f : a -> b -> c) : b -> a -> c :=
@@ -102,14 +102,14 @@ Section SparkData.
     (* Uh, so this means every piece of data has an additional representation at type top,
      * and it's the same for every type of data. I guess this makes some sense in that we
      * seem to require a blob/string representation for data inside .. anyways. *)
-    | _, Top₀ => Some (sblob d)
-    | dunit, Unit₀ => Some snull
-    | dnat i, Nat₀ => Some (sint i)
-    | dbool b, Bool₀ => Some (sbool b)
-    | dstring s, String₀ => Some (sstring s)
+    | _, Top₀ => Some (Sblob d)
+    | dunit, Unit₀ => Some Snull
+    | dnat i, Nat₀ => Some (Sint i)
+    | dbool b, Bool₀ => Some (Sbool b)
+    | dstring s, String₀ => Some (Sstring s)
     | dcoll xs, (Coll₀ et) =>
       let listo := map (flip typed_data_to_sdata_0 et) xs in
-      lift sarray (listo_to_olist listo)
+      lift Sarray (listo_to_olist listo)
     | drec fs, Rec₀ Closed ft =>
       let fix convert_fields ds ts :=
           match ds, ts with
@@ -122,7 +122,7 @@ Section SparkData.
             | _, _ => None
             end
           end in
-      lift (fun d => srow d None) (convert_fields fs ft)
+      lift (fun d => Srow d None) (convert_fields fs ft)
     | drec fs, Rec₀ Open ft =>
     (* Put typed fields in typed part, leftover fields in .. part *)
       let fix convert_known_fields ds ts us :=
@@ -142,23 +142,23 @@ Section SparkData.
       (* I'm not sure the dotdot fields are in the correct order, might need to sort them. *)
       match convert_known_fields fs ft nil with
       | Some (typed_fields, dotdot) =>
-        Some (srow typed_fields (Some (drec dotdot)))
+        Some (Srow typed_fields (Some (drec dotdot)))
       | None => None
       end
     | dleft l, Either₀ lt rt =>
       match typed_data_to_sdata_0 l lt with
-      | Some l => Some (srow (("$left"%string, l)::("$right"%string, snull)::nil) None)
+      | Some l => Some (Srow (("$left"%string, l)::("$right"%string, Snull)::nil) None)
       | None => None
       end
     | dright r, Either₀ lt rt =>
       match typed_data_to_sdata_0 r rt with
-      | Some r => Some (srow (("$left"%string, snull)::("$right"%string, r)::nil) None)
+      | Some r => Some (Srow (("$left"%string, Snull)::("$right"%string, r)::nil) None)
       | None => None
       end
     | dbrand bs v, Brand₀ bts =>
       let type := brand_content_rtype bts in
       match typed_data_to_sdata_0 v type with
-      | Some data => Some (srow (("$type"%string, sarray (map sstring bs))
+      | Some data => Some (Srow (("$type"%string, Sarray (map Sstring bs))
                                    ::("$data"%string, data)::nil)
                                 None)
       | None => None
@@ -173,7 +173,7 @@ Section SparkData.
     exists x : sdata, typed_data_to_sdata d ⊤ = Some x.
   Proof.
     intros; subst; unfold typed_data_to_sdata; simpl;
-      exists (sblob d);
+      exists (Sblob d);
       destruct d; reflexivity.
   Qed.
 
@@ -198,8 +198,20 @@ Section SparkData.
       elim IHd; intros.
       unfold typed_data_to_sdata in H0; simpl in H0.
       rewrite H0.
-      exists ((srow (("$left"%string, x0) :: ("$right"%string, snull) :: nil) None)).
-      reflexivity.
+      eauto.
+    - destruct (istop r); try (subst; apply top_typed_data_to_some_sdata).
+      destruct (data_type_dright_inv e H) as [? [t ?]]; subst.
+      unfold typed_data_to_sdata; simpl.
+      specialize (IHd t).
+      inversion H. subst.
+      assert (τr = t) by apply (rtype_fequal H3).
+      subst.
+      specialize (IHd H2).
+      elim IHd; intros.
+      unfold typed_data_to_sdata in H0; simpl in H0.
+      rewrite H0.
+      eauto.
+    - destruct (istop r); try (subst; apply top_typed_data_to_some_sdata).
   Admitted.
 
 End SparkData.
