27c51ccc3f5a3e863fbb6c8d6232ae06d340eb44
Jerome Simeon <simeon@us.ibm.com>
2016-10-17 22:28:25 -0400

    Fix sort-by bug in runtime ; q22 works end to end

--- runtime/javascript/qcert-runtime.js
+++ runtime/javascript/qcert-runtime.js
@@ -96,27 +96,24 @@ function equal(v1, v2) {
 }
 function compareOfSingleCriteria(sc) {
     if ("asc" in sc) {
-	return function(a,b) { return compare(a[sc.asc], b[sc.asc]); }
+	java.lang.System.err.println("Sorting in ascending order");
+	return function(a,b) { return compare(deref(a,sc['asc']), deref(b,sc['asc'])); }
     } else if ("desc" in sc) { 
-	return function(a,b) { return -(compare(a[sc.desc], b[sc.desc])); }
+	java.lang.System.err.println("Sorting in descending order");
+	return function(a,b) { return -(compare(deref(a,sc['desc']), dref(b,sc['desc']))); }
     } else {
+	java.lang.System.err.println("Sorting in weird order");
 	return function (a,b) { return compare(a,b); }
     } /* Default to just comparing values */
 }
-function sort(v,scl) {
+function sort(b,scl) {
     var result = [ ];
-    if (scl.length == 0) { return v; } // Check for no sorting criteria
-    var compareFun = function(a,b) {
-	(compareOfSingleCriteria(scl[0]))(a,b);
-    };
-    for (var i=0; i<b.length; i++) {
-        var v = b[i];
-        var dup = false;
-        for (var j=0; j<result.length;j++) {
-          if (equal(v,result[j])) { dup = true; break; }
-        }
-        if (!(dup)) { result.push(v); } else { dup = false; }
-    }
+    if (scl.length == 0) { return b; } // Check for no sorting criteria
+    var compareFun =
+	(compareOfSingleCriteria(scl[0]));
+    ;
+    result = b.slice(); /* Sorting in place leads to inconsistencies, notably as it re-orders the input WM in the middle of processing */
+    result.sort(compareFun);
     return result;
 }
 function flatten(aOuter) {
