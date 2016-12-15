68890a5f469b10ff2ade50a8d7ee595f7d85932a
Avi Shinnar <shinnar@us.ibm.com>
2016-12-15 16:45:51 -0500

    Add column, dataset, and dnnrc size functions: part2

---
+++ coq/DNNRC/Calculus/DNNRCSize.v
@@ -0,0 +1,55 @@
+(*
+ * Copyright 2015-2016 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *)
+
+Section size.
+
+  Require Import List.
+  Require Import Omega.
+  Require Import BasicRuntime DNNRC.
+
+  Context {fruntime:foreign_runtime}.
+  Context {A plug_type:Set}.
+
+  Context (plug_size:plug_type->nat).
+  
+  Fixpoint dnnrc_size (d:dnnrc A plug_type)
+    := match d with
+       | DNNRCVar _ _ => 1
+       | DNNRCConst _ _ => 1
+       | DNNRCBinop _ _ d1 d2 => S (dnnrc_size d1 + dnnrc_size d2)
+       | DNNRCUnop _ _ d0 => S (dnnrc_size d0)
+       | DNNRCLet _ _ d1 d2 => S (dnnrc_size d1 + dnnrc_size d2)
+       | DNNRCFor _ _ d1 d2 => S (dnnrc_size d1 + dnnrc_size d2)
+       | DNNRCIf _ d1 d2 d3 => S (dnnrc_size d1 + dnnrc_size d2 + dnnrc_size d3)
+       | DNNRCEither _ d1 _ d2 _ d3 => S (dnnrc_size d1 + dnnrc_size d2 + dnnrc_size d3)
+       | DNNRCGroupBy _ _ _ d0 => S (dnnrc_size d0)
+       | DNNRCCollect _ d0 => S (dnnrc_size d0)
+       | DNNRCDispatch _ d0 => S (dnnrc_size d0)
+       | DNNRCAlg _ pt sdl => S (plug_size pt + (fold_left (fun acc sc => dnnrc_size (snd sc) + acc) sdl 0))
+       end.
+
+  Lemma dnnrc_size_nzero (d:dnnrc A plug_type) : dnnrc_size d <> 0.
+  Proof.
+    induction d; simpl; omega.
+  Qed.
+
+End size.
+
+(*
+*** Local Variables: ***
+*** coq-load-path: (("../../../coq" "Qcert")) ***
+*** End: ***
+*)
---
+++ coq/DNNRC/Calculus/DatasetSize.v
@@ -0,0 +1,70 @@
+(*
+ * Copyright 2015-2016 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *)
+
+Section size.
+  
+  Require Import List.
+  Require Import Omega.
+  Require Import BasicRuntime ForeignType Dataset.
+
+  Context {fruntime:foreign_runtime}.
+  Context {ftype: foreign_type}.
+
+  Fixpoint column_size (c:column)
+    := match c with
+       | CCol _ => 1
+       | CDot _ c0 => S (column_size c0)
+       | CLit _ => 1
+       | CPlus c1 c2 => S (column_size c1 + column_size c2)
+       | CEq c1 c2 => S (column_size c1 + column_size c2)
+       | CLessThan  c1 c2 => S (column_size c1 + column_size c2)
+       | CNeg c0 => S (column_size c0)
+       (* NOTE we actually codegen to a UDF for this, not Spark's printing *)
+       | CToString c0 => S (column_size c0)
+       | CSConcat c1 c2 => S (column_size c1 + column_size c2)
+       | CUDFCast _ c0 => S (column_size c0)
+       | CUDFUnbrand _ c0 => S (column_size c0)
+       end.
+
+  Fixpoint dataset_size (d:dataset)
+    := match d with
+       | DSVar _ => 1
+       | DSSelect scl d0 => S ( 
+                                (fold_left (fun acc sc => column_size (snd sc) + acc) scl 0)
+                                  + dataset_size d0)
+       | DSFilter c0 d0 => S (column_size c0 + dataset_size d0)
+       | DSCartesian d1 d2 => S (dataset_size d1 + dataset_size d2)
+       | DSExplode _ d0 => S (dataset_size d0)
+       end.
+
+
+  Lemma column_size_nzero (c:column) : column_size c <> 0.
+  Proof.
+    induction c; simpl; omega.
+  Qed.
+
+    Lemma dataset_size_nzero (d:dataset) : dataset_size d <> 0.
+  Proof.
+    induction d; simpl; omega.
+  Qed.
+
+End size.
+
+(*
+*** Local Variables: ***
+*** coq-load-path: (("../../../coq" "Qcert")) ***
+*** End: ***
+*)
