a9b3dc849a1df4d6a8c0ee6537d6cf8639877ac7
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2017-02-13 15:37:50 +0000

    Emit IRs

--- tests/spark2/run.sh
+++ tests/spark2/run.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 
 mkdir -p test01_nnrc/src/main/scala/
-qcert -schema test01.schema -target spark_dataset -dir-target test01_nnrc/src/main/scala test01.camp
+mkdir -p test01_nnrc/irs/
+qcert -emit-all -dir test01_nnrc/irs -schema test01.schema -target spark_dataset -dir-target test01_nnrc/src/main/scala test01.camp
 pushd test01_nnrc
 sbt assembly
 spark-submit --class R01 target/scala-2.11/test01_nnrc-assembly-0.1-SNAPSHOT.jar ../test01.sio
