4f2673eb1d59760bcd13e335a00ee80c3ef2d016
Joshua Auerbach <joshuaauerbachwatson@gmail.com>
2017-05-05 18:14:06 -0400

    Tweaks to make it easier to run lots of queries through new support

--- sqlParser/src/org/qcert/experimental/sql/SqlppEncoder.java
+++ sqlParser/src/org/qcert/experimental/sql/SqlppEncoder.java
@@ -17,6 +17,9 @@ package org.qcert.experimental.sql;
 
 import java.io.FileReader;
 import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.asterix.common.exceptions.CompilationException;
@@ -62,20 +65,58 @@ public class SqlppEncoder {
 	 * TODO move this to its own source file when it becomes elaborate enough
 	 */
 	public static void main(String[] args) throws Exception {
+		if (args.length == 3 && isNumber(args[1]) && isNumber(args[2]))
+			args = generateRange(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]), ".sql");
 		for (String arg : args) {
-			Reader input = new FileReader(arg);
-			List<Statement> stmts = parse(input);
-			System.out.println(encode(stmts, true));
+			System.out.print(arg + ": ");
+			List<Statement> stmts;
+			try {
+				stmts = parseFile(arg);
+			} catch (CompilationException e) {
+				System.out.println(e.getMessage());
+				continue;
+			}
+			try { 
+				System.out.println(encode(stmts, true));
+			} catch (UnsupportedOperationException e) {
+				System.out.println(e.toString());
+			}
 		}
 	}
 
 	/**
-	 * Parse SQL source provided via a Reader.
-	 * @param input the Reader providing the source
+	 * Generate a range of arguments from a stem, a suffix, and two numeric range limits
+	 * @param stem the stem to use
+	 * @param start the start of the range
+	 * @param end the end of the range
+	 * @param suffix the suffix to use
+	 * @return
+	 */
+	private static String[] generateRange(String stem, int start, int end, String suffix) {
+		List<String> range = new ArrayList<>();
+		for (int i = start; i <= end; i++)
+			range.add(stem + i + suffix);
+		return range.toArray(new String[range.size()]);
+	}
+
+	/** Determine if a string looks like an integer number */
+	private static boolean isNumber(String string) {
+		for (int i = 0; i < string.length(); i++)
+			if (!Character.isDigit(string.charAt(i)))
+				return false;
+		return true;
+	}
+
+	/**
+	 * Parse SQL source provided in a File
+	 * @param file the name of the file containing the source
 	 * @return the parsed statement(s) as a List<Statement>
 	 */
-	public static List<Statement> parse(Reader input) throws Exception {
-		return new SqlppParserFactory().createParser(input).parse();
+	public static List<Statement> parseFile(String file) throws Exception {
+		// Note: SqlppParserFactory has a method that will make a parser from a Reader but we do not use it because it does not
+		// set up the error handling state fully, so you get an NPE instead of an informative error on things like syntax errors.
+		String query = new String(Files.readAllBytes(Paths.get(file)));
+		return parse(query);
 	}
 
 	/**
