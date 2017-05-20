ae9b02cbf9070fd7962dbbc4ab7868168ca10b9f
Joshua Auerbach <joshuaauerbachwatson@gmail.com>
2017-05-20 09:22:40 -0400

    Split SQL++ from SQL preparatory to letting the languages diverge.

--- sqlppParser/src/org/qcert/sqlpp/FixupValueKeyword.java
+++ sqlppParser/src/org/qcert/sqlpp/FixupValueKeyword.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.qcert.experimental.sql;
+package org.qcert.sqlpp;
 
 import java.util.Iterator;
 import java.util.List;
--- sqlppParser/src/org/qcert/sqlpp/SqlppEncodingVisitor.java
+++ sqlppParser/src/org/qcert/sqlpp/SqlppEncodingVisitor.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.qcert.experimental.sql;
+package org.qcert.sqlpp;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
