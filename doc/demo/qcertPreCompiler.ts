bfee9144d31457f10034e455dc1a2906090df591
Joshua Auerbach <joshuaauerbachwatson@gmail.com>
2017-09-17 14:34:49 -0700

    Incorporate whisk action into demo (not the default yet).
    To enable the whisk action at the moment you have to switch the server
    order as described at the top of qcertPreCompiler.ts and rebuild the
    demo.

--- doc/demo/qcertPreCompiler.ts
+++ doc/demo/qcertPreCompiler.ts
@@ -15,8 +15,19 @@
  */
 
 // List server hosts in the order they should be attempted.  It is a good idea (but not required) to put localhost first so that
-// new functionality can be tested without updating remote server hosts.
-var serverHosts = [ "localhost", "35.164.159.76" ];
+// new functionality can be tested without updating remote server hosts.   The list can contain two distinct string formats.  
+// (1) A string that does not contain forward slashes is assumed to be a host name or
+// address and a long-running server is assumed to be at that host on port 9879.
+// (2) A string containing forward slashes is assumed to be a URL; the URL is used to invoke a whisk web action providing the server
+// functionality.
+// NOTE: the usually-available long-running server at 35.164.159.76 is currently listed AHEAD of the whisk action.  To test the whisk
+// action, change the order.  The long-running server will probably be switched off once we have confidence in the whisk action.
+
+var serverHosts = [ 
+    "localhost", 
+    "35.164.159.76",
+    "https://openwhisk.ng.bluemix.net/api/v1/web/JoshAuerbachThoughts_test/default/qcertJavaService.json", 
+    ];
 
 function preProcess(text: string, verb: string, callback: (result: string) => any) {
     var next = function() {
@@ -30,14 +41,22 @@ function preProcess(text: string, verb: string, callback: (result: string) => an
 function makeHandler(text: string, verb: string, host: string, success: (result: string) => any, failure: () => any) {
     return function() {
         console.log("Handler invoked for host " + host);
-        var url = "http://" + host + ":9879?verb=" + verb;
+        var whisk = false;
+        var url;
+        if (host.indexOf('/') > 0) {
+            whisk = true;
+            url = host;
+            text = whiskify(verb, text);
+        } else
+            url = "http://" + host + ":9879?verb=" + verb;
         var request = new XMLHttpRequest();
         request.open("POST", url, true);
-        request.setRequestHeader("Content-Type", "text/plain");
+        request.setRequestHeader("Content-Type", whisk ? "application/json" : "text/plain");
         request.onloadend = function() {
             if (request.status == 200) {
                 console.log("Success with verb " + verb + " at host " + host);
-                success(request.responseText);
+                var response = whisk ? dewhiskify(request.responseText) : request.responseText;
+                success(response);
             } else {
                 console.log("Failure with verb " + verb + " at host " + host);
                 failure();
@@ -49,6 +68,15 @@ function makeHandler(text: string, verb: string, host: string, success: (result:
         } catch (e) {
         }
     }
+}
+
+function whiskify(verb: string, text: string) {
+    return JSON.stringify({verb: verb, arg: text});
+}
+
+function dewhiskify(response: string) {
+    var obj = JSON.parse(response);
+    return obj.response;
 }
 
 function combineInputAndSchema(input: string, schema: string) {
