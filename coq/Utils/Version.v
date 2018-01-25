cdb4bfadf543425f58f1c0386f9908dd66d85d30
Jerome Simeon <simeon@us.ibm.com>
2018-01-24 23:46:11 -0500

    Upgrade to version 1.0.2

--- coq/Utils/Version.v
+++ coq/Utils/Version.v
@@ -20,7 +20,7 @@ Require Import String.
 
 Section Version.
   (** Variables are defined as strings *)
-  Definition qcert_version := "1.0.1"%string.
+  Definition qcert_version := "1.0.2"%string.
 
 End Version.
 
