75ff811c89a28cb6bd341369c83c0c716a98e96b
Jerome Simeon <simeon@us.ibm.com>
2018-02-19 09:22:03 -0500

    Up to version 1.0.5

--- coq/Version.v
+++ coq/Version.v
@@ -20,7 +20,7 @@ Require Import String.
 
 Section Version.
   (** Variables are defined as strings *)
-  Definition qcert_version := "1.0.4-dev"%string.
+  Definition qcert_version := "1.0.5"%string.
 
 End Version.
 
