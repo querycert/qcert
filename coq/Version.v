4095ca212e8ab67e655e43afb7ce5e2b96a09bf8
Jerome Simeon <jeromesimeon@me.com>
2019-09-30 13:35:09 -0400

    chore(1.4.0) New version tags
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- coq/Version.v
+++ coq/Version.v
@@ -20,7 +20,7 @@ Require Import String.
 
 Section Version.
   (** Variables are defined as strings *)
-  Definition qcert_version := "1.3.0"%string.
+  Definition qcert_version := "1.4.0"%string.
 
 End Version.
 
