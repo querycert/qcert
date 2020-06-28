f122f292af6fbada4503e7f99812418613794671
Patrik Keller <patrik@keller-re.de>
2020-06-28 17:02:21 +0200

    feature(wasm) Add preliminary wasm backend
    Signed-off-by: Patrik Keller <patrik@keller-re.de>

--- compiler/util/wasm.ml
+++
@@ -1,5 +0,0 @@
-(* XXX This is a placeholder *)
-
-type ast = unit
-let eval fe j w env = raise (Failure "Cannot eval WASM AST")
-let imp_ejson_to_wasm_ast fe fer i =  raise (Failure "Cannot compile to WASM AST")
