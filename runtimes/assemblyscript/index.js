cea49eb0e6f9f2164171890c4b9da1d4eb7d99eb
Patrik Keller <patrik@keller-re.de>
2020-07-07 17:12:56 +0200

    Link runtime with compiled module and execute.
    This commit add a PoC engine that executes a compiled to wasm qcert
    query on NodeJS. It dynamically links the IMP runtime that we implement
    in AssemblyScript and compile to a separate wasm module.

--- runtimes/assemblyscript/index.js
+++ runtimes/assemblyscript/index.js
@@ -1,5 +1,27 @@
-const fs = require("fs");
-const loader = require("@assemblyscript/loader");
-const imports = { /* imports go here */ };
-const wasmModule = loader.instantiateSync(fs.readFileSync(__dirname + "/build/optimized.wasm"), imports);
-module.exports = wasmModule.exports;
+'use strict';
+
+const fs = require('fs');
+const engine = require('./lib/engine.js');
+
+async function main(runtime, module, input, expected) {
+  console.log("invoke:");
+  console.log({runtime, module, input, expected});
+  let rt = fs.readFileSync(runtime);
+  let mod = fs.readFileSync(module);
+  let arg = JSON.parse(fs.readFileSync(input));
+  console.log("input:");
+  console.log(arg);
+  let exp = JSON.parse(fs.readFileSync(expected));
+  console.log("expected:");
+  console.log(exp);
+  let res = await engine.invoke(rt, mod, arg);
+  console.log("output:");
+  console.log(res);
+}
+
+const rt = process.argv[2];
+const mod = process.argv[3];
+const arg = process.argv[4];
+const exp = process.argv[5];
+
+main(rt, mod, arg, exp);
