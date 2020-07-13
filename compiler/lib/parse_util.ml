1aeaffb4e9de5ae9e782d34724b79f54390c4bb4
Jerome Simeon <jeromesimeon@me.com>
2020-07-13 14:16:01 -0400

    WIP(imp) Generalizes runtime calls in imp parser
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- compiler/lib/parse_util.ml
+++ compiler/lib/parse_util.ml
@@ -30,19 +30,17 @@ let parse parser lexer buf =
       parser lexer buf
     with
     | LexError msg ->
-	begin
-	  let pos = buf.Lexing.lex_start_p in
-	  let msg = Printf.sprintf "At line %d column %d: %s%!" pos.Lexing.pos_lnum (pos.Lexing.pos_cnum - pos.Lexing.pos_bol) msg in
-	  raise (LexError msg)
-	end
+	      begin
+	        let pos = buf.Lexing.lex_start_p in
+	        let msg = Printf.sprintf "At line %d column %d: %s%!" pos.Lexing.pos_lnum (pos.Lexing.pos_cnum - pos.Lexing.pos_bol) msg in
+	        raise (LexError msg)
+	      end
     | _ ->
-	begin
-	  let pos = buf.Lexing.lex_start_p in
-	  let msg = Printf.sprintf "At line %d column %d: syntax error%!" pos.Lexing.pos_lnum (pos.Lexing.pos_cnum - pos.Lexing.pos_bol) in
-	  raise (LexError msg)
-	end
-
-
+	      begin
+	        let pos = buf.Lexing.lex_start_p in
+	        let msg = Printf.sprintf "At line %d column %d: syntax error%!" pos.Lexing.pos_lnum (pos.Lexing.pos_cnum - pos.Lexing.pos_bol) in
+	        raise (LexError msg)
+	      end
 
 (******************)
 (* Specific Parse *)
