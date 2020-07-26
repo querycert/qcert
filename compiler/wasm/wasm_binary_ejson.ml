662c64fe6be834d56ee44bd66053872ee87b53e0
Patrik Keller <patrik@keller-re.de>
2020-07-26 13:40:55 +0200

    Binary EJson (Ocaml)

--- compiler/wasm/wasm_binary_ejson.ml
+++ compiler/wasm/wasm_binary_ejson.ml
@@ -37,11 +37,12 @@ let cejson_to_bytes : _ cejson -> bytes =
   | Coq_cejforeign _ -> unsupported "ejson encode: foreign"
 
 let ejson_to_bytes x =
-  (* collect byte segments in reverse order and track the overall length *)
+  (* collect byte segments in reverse order and track the overall length. *)
   let segments, size = ref [], ref 0 in
+  let open Bytes in
   let append x =
     segments := x :: !segments;
-    size := Bytes.length x + !size
+    size := length x + !size
   in
   let rec f = function
   | Coq_ejnull -> append (cejson_to_bytes Coq_cejnull)
@@ -50,7 +51,25 @@ let ejson_to_bytes x =
   | Coq_ejbigint x -> append (cejson_to_bytes (Coq_cejbigint x))
   | Coq_ejstring x -> append (cejson_to_bytes (Coq_cejstring x))
   | Coq_ejforeign x -> append (cejson_to_bytes (Coq_cejforeign x))
-  | _ -> failwith "ejson not supported" (* TODO *)
+  | Coq_ejarray x ->
+    let b = create 5 in
+    set_uint8 b 0 6;
+    set_int32_le b 1 (List.length x |> Int32.of_int);
+    append b;
+    List.iter f x
+  | Coq_ejobject x ->
+    let b = create 5 in
+    set_uint8 b 0 7;
+    set_int32_le b 1 (List.length x |> Int32.of_int);
+    List.iter (fun (k, v) ->
+        let k = Util.string_of_char_list k in
+        let n = String.length k in
+        let b = create (n + 4) in
+        set_int32_le b 0 (Int32.of_int n);
+        blit_string k 0 b 4 n;
+        append b;
+        f v
+      ) x
   in
   f x;
   (* create to-be-returned bytes and "fill" in reverse order. *)
@@ -66,4 +85,45 @@ let ejson_to_bytes x =
   f !segments;
   b
 
-let ejson_of_bytes b = Coq_ejnull (* TODO *)
+let ejson_of_bytes b =
+  let p =
+    let pointer = ref 0 in
+    fun increment_by ->
+      let r = !pointer in
+      pointer := !pointer + increment_by;
+      r
+  in
+  let open Bytes in
+  let rec f () =
+    match get_int8 b (p 1) with
+    | 0 -> Coq_ejnull
+    | 1 -> Coq_ejbool false
+    | 2 -> Coq_ejbool true
+    | 3 ->
+      let x = get_int64_le b (p 8) |> Int64.float_of_bits in
+      Coq_ejnumber x
+    | 4 ->
+      let x = get_int64_le b (p 8) |> Int64.float_of_bits |> int_of_float in
+      Coq_ejbigint x
+    | 5 ->
+      let n = get_int32_le b (p 4) |> Int32.to_int in
+      Coq_ejstring (sub_string b (p n) n |> Util.char_list_of_string)
+    | 6 ->
+      let n =
+        get_int32_le b (p 4)
+        |> Int32.to_int
+      in
+      Coq_ejarray (List.init n (fun _ -> f ()))
+    | 7 ->
+      let n =
+        get_int32_le b (p 4)
+        |> Int32.to_int
+      in
+      Coq_ejobject (List.init n (fun _ ->
+          let n = get_int32_le b (p 4) |> Int32.to_int in
+          let key = sub_string b (p n) n |> Util.char_list_of_string in
+          key, f ()
+        ))
+    | _ -> failwith "ejson_of_bytes: unknown tag"
+  in
+  f ()
