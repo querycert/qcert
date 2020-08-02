7952c4a6826a244f738057cdec071021863f20ec
Patrik Keller <patrik@keller-re.de>
2020-08-02 18:28:59 +0200

    Support multi function Imp.lib

--- compiler/wasm/wasm_ast.ml
+++ compiler/wasm/wasm_ast.ml
@@ -7,3 +7,16 @@ include Wasm_backend.Make(struct
     include Imp
     include ImpEJson
   end)
+
+(* Qcert compiles queries. There is only one function per [Imp.lib].
+ * We name this single function 'qcert_main'.
+ * We patch the generic translate and eval functions accordingly.
+ *)
+
+let main = Util.char_list_of_string "qcert_main"
+
+let imp_ejson_to_wasm_ast = function
+  | [ _, fn ] -> imp_ejson_to_wasm_ast [ main, fn ]
+  | _ -> failwith "Wasm_ast.imp_ejson_to_wasm_ast: single function expected"
+
+let eval hierarchy module_ env = eval hierarchy module_ main env
