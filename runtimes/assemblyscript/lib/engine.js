045617e52817bf8831666e77dc848ba3caf0b966
Patrik Keller <patrik@keller-re.de>
2020-08-10 20:20:41 +0200

    Fix assemblyscript tests

--- runtimes/assemblyscript/lib/engine.js
+++ runtimes/assemblyscript/lib/engine.js
@@ -7,9 +7,9 @@ async function invoke(runtime, module, hierarchy, arg) {
   let rt = await loader.instantiate(runtime);
   let m = await loader.instantiate(module, { runtime: rt.instance.exports });
   let hierarchy_ptr = enc.write(rt, hierarchy);
-  let arg_ptr = enc.write(rt, arg);
+  let arg_ptr = enc.write(rt, arg); // not using binary encoding.
   let res_ptr = m.exports.qcert_main(hierarchy_ptr, arg_ptr);
-  let res = enc.read(rt, res_ptr);
+  let res = enc.read(rt, res_ptr); // not using binary encoding.
   return res;
 }
 
