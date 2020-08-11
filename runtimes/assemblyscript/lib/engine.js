11c030f89da31d6310baa9e84ca5f2c439a536f7
Patrik Keller <patrik@keller-re.de>
2020-08-11 18:12:06 +0200

    Invoke compiled wasm with binary encoded EJson arguments und result.

--- runtimes/assemblyscript/lib/engine.js
+++ runtimes/assemblyscript/lib/engine.js
@@ -8,15 +8,12 @@ function write(mod, value) {
   let value_bin = bin.ejson_to_bytes(value); // ejson --JS--> binary
   let bytes_ptr = __retain(__alloc(value_bin.byteLength)); // alloc runtime memory
   Buffer.from(value_bin).copy(Buffer.from(memory.buffer, bytes_ptr)); // copy binary value
-  let value_ptr = ejson_of_bytes(bytes_ptr); // binary --runtime--> ejson
-  __release(bytes_ptr); // free temp memory
-  return value_ptr;
+  return bytes_ptr;
 }
 
 function read(mod, ptr) {
   let { memory, ejson_to_bytes } = mod.exports;
-  let bytes_ptr = ejson_to_bytes(ptr); // ejson --runtime--> binary
-  let value = bin.ejson_of_bytes(memory.buffer, bytes_ptr); // binary --JS--> ejson
+  let value = bin.ejson_of_bytes(memory.buffer, ptr); // binary --JS--> ejson
   return value;
 }
 
