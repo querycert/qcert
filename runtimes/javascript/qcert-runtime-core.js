3a904463d010d735623f598d376d88391fa63b0c
Jerome Simeon <jeromesimeon@me.com>
2022-05-25 23:37:47 -0400

    fix(runtime): Cleanup JavaScript runtime, fixes bug in union
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- runtimes/javascript/qcert-runtime-core.js
+++ runtimes/javascript/qcert-runtime-core.js
@@ -48,11 +48,11 @@ function isRight(v) {
   return Object.prototype.hasOwnProperty.call(v,'$right');
 }
 function sub_brand(b1,b2) {
-  var bsub=null;
-  var bsup=null;
-  var inheritanceUnbox = isBoxColl(inheritance)?unboxColl(inheritance):inheritance;
-  var length = inheritanceUnbox.length;
-  for (var i=0; i<length; i=i+1) {
+  let bsub = null;
+  let bsup = null;
+  const inheritanceUnbox = isBoxColl(inheritance) ? unboxColl(inheritance) : inheritance;
+  const length = inheritanceUnbox.length;
+  for (let i = 0; i < length; i=i+1) {
     bsub = inheritanceUnbox[i].sub;
     bsup = inheritanceUnbox[i].sup;
     if ((b1 === bsub) && (b2 === bsup)) { return true; }
@@ -69,7 +69,8 @@ function equal(v1, v2) {
   return compare(v1, v2) === 0;
 }
 function compare(v1, v2) {
-  var t1 = typeof v1, t2 = typeof v2;
+  const t1 = typeof v1;
+  const t2 = typeof v2;
   if (t1 === 'object' && v1 !== null) {
     if (isBoxColl(v1)) {
 	    v1 = unboxColl(v1).slice(0, collLength(v1));
@@ -83,7 +84,8 @@ function compare(v1, v2) {
   if (t1 != t2) {
     return t1 < t2 ? -1 : +1;
   }
-  var a1 = {}.toString.apply(v1), a2 = {}.toString.apply(v2);
+  const a1 = {}.toString.apply(v1);
+  const a2 = {}.toString.apply(v2);
   if (a1 != a2) {
     return a1 < a2 ? -1 : +1;
   }
@@ -94,22 +96,22 @@ function compare(v1, v2) {
     v2.sort(compare);
   }
   if (t1 === 'object') {
-    var fields1 = [];
-    var fields2 = [];
-    for (var f1 in v1) { fields1.push(f1); }
-    for (var f2 in v2) { fields2.push(f2); }
-    fields1 = fields1.sort(compare);
-    fields2 = fields2.sort(compare);
-    for (var i = 0; i < fields1.length; i=i+1) {
+    const fields1 = [];
+    const fields2 = [];
+    for (const f1 in v1) { fields1.push(f1); }
+    for (const f2 in v2) { fields2.push(f2); }
+    fields1.sort(compare);
+    fields2.sort(compare);
+    for (let i = 0; i < fields1.length; i=i+1) {
       if (!(Object.prototype.hasOwnProperty.call(v2,fields1[i]))) {
         return -1;
       }
-      var fc = compare(v1[fields1[i]], v2[fields1[i]]);
+      const fc = compare(v1[fields1[i]], v2[fields1[i]]);
       if (fc != 0) {
         return fc;
       }
     }
-    for (var i = 0; i < fields2.length; i=i+1) {
+    for (let i = 0; i < fields2.length; i=i+1) {
       if (!(Object.prototype.hasOwnProperty.call(v1,fields2[i]))) {
         return +1;
       }
@@ -124,11 +126,11 @@ function compare(v1, v2) {
 
 /* Record */
 function recConcat(r1, r2) {
-  var result = { };
-  for (var key2 in r2) {
+  const result = { };
+  for (const key2 in r2) {
     result[key2] = r2[key2];
   }
-  for (var key1 in r1) {
+  for (const key1 in r1) {
     if (!(Object.prototype.hasOwnProperty.call(r2,key1))) {
       result[key1] = r1[key1];
     }
@@ -136,11 +138,11 @@ function recConcat(r1, r2) {
   return result;
 }
 function recMerge(r1, r2) {
-  var result = { };
-  for (var key1 in r1) {
+  const result = { };
+  for (const key1 in r1) {
     result[key1] = r1[key1];
   }
-  for (var key2 in r2) {
+  for (const key2 in r2) {
     if (Object.prototype.hasOwnProperty.call(r1,key2)) {
       if (!equal(r1[key2], r2[key2])) {
         return [ ];
@@ -152,8 +154,8 @@ function recMerge(r1, r2) {
   return [ result ];
 }
 function recRemove(r, f) {
-  var result = { };
-  for (var key in r) {
+  const result = { };
+  for (let key in r) {
     if (key != f) {
       result[key] = r[key];
     }
@@ -161,8 +163,8 @@ function recRemove(r, f) {
   return result;
 }
 function recProject(r1, p2) {
-  var result = { };
-  for (var key1 in r1) {
+  const result = { };
+  for (let key1 in r1) {
     if (!(p2.indexOf(key1) === -1)) {
       result[key1] = r1[key1];
     }
@@ -184,7 +186,7 @@ function arrayLength(v) {
   return BigInt(v.$length);
 }
 function arrayPush(v1,v2) {
-  var content1 = unboxColl(v1);
+  const content1 = unboxColl(v1);
   if (content1.length !== collLength(v1)) {
 	  content1 = content1.slice(0, collLength(length));
   }
@@ -192,7 +194,7 @@ function arrayPush(v1,v2) {
   return boxColl(content1);
 }
 function arrayAccess(v1,v2) {
-  var content1 = unboxColl(v1);
+  const content1 = unboxColl(v1);
   return content1[v2];
 }
 
@@ -230,16 +232,16 @@ function unbrand(v) {
   throw new Error('TypeError: unbrand called on non-object');
 }
 function cast(brands,v) {
-  var brandsUnbox = isBoxColl(brands) ? unboxColl(brands) : brands;
-  var type = isBoxColl(v.$class) ? unboxColl(v.$class) : v.$class;
+  const brandsUnbox = isBoxColl(brands) ? unboxColl(brands) : brands;
+  const type = isBoxColl(v.$class) ? unboxColl(v.$class) : v.$class;
   if (brandsUnbox.length === 1 && brandsUnbox[0] === 'Any') { /* cast to top of inheritance is built-in */
     return boxLeft(v);
   }
   brands:
-  for (var i in brandsUnbox) {
-    var b = brandsUnbox[i];
-    for (var j in type) {
-      var t = type[j];
+  for (const i in brandsUnbox) {
+    const b = brandsUnbox[i];
+    for (const j in type) {
+      const t = type[j];
       if (equal(t,b) || sub_brand(t,b)) {
         continue brands;
       }
@@ -253,18 +255,18 @@ function cast(brands,v) {
 
 /* Collection */
 function iterColl(b, f) {
-  var content = unboxColl(b);
-  for (let i = 0; i < collLength(b); i++) {
+  const content = unboxColl(b);
+  for (let i = 0; i < collLength(b); i=i+1) {
 	  f(content[i]);
   }
 }
 function distinct(b) {
-  var result = [ ];
-  var content = unboxColl(b);
-  for (var i=0; i < collLength(b); i=i+1) {
-    var v = content[i];
-    var dup = false;
-    for (var j=i+1; j < collLength(b); j=j+1) {
+  const result = [ ];
+  const content = unboxColl(b);
+  for (let i = 0; i < collLength(b); i=i+1) {
+    const v = content[i];
+    let dup = false;
+    for (let j = i+1; j < collLength(b); j=j+1) {
       if (equal(v,content[j])) { dup = true; break; }
     }
     if (!(dup)) { result.push(v); } else { dup = false; }
@@ -272,7 +274,7 @@ function distinct(b) {
   return boxColl(result);
 }
 function singleton(v) {
-  var content = unboxColl(v);
+  const content = unboxColl(v);
   if (collLength(v) === 1) {
     return boxLeft(content[0]);
   } else {
@@ -280,41 +282,43 @@ function singleton(v) {
   }
 }
 function flatten(aOuter) {
-  var result = [ ];
-  var aOuterContent = unboxColl(aOuter);
-  for (var iOuter=0, nOuter=collLength(aOuter); iOuter<nOuter; iOuter = iOuter+1) {
-    var aInner = aOuterContent[iOuter];
-    var aInnerContent = unboxColl(aInner);
-    for (var iInner=0, nInner=collLength(aInner); iInner<nInner; iInner = iInner+1) {
+  const result = [ ];
+  const aOuterContent = unboxColl(aOuter);
+  const nOuter = collLength(aOuter);
+  for (let iOuter = 0; iOuter < nOuter; iOuter = iOuter+1) {
+    const aInner = aOuterContent[iOuter];
+    const aInnerContent = unboxColl(aInner);
+    const nInner = collLength(aInner);
+    for (let iInner = 0; iInner < nInner; iInner = iInner+1) {
       result.push(aInnerContent[iInner]);
     }
   }
   return boxColl(result);
 }
 function union(b1, b2) {
-  var content1 = unboxColl(b1);
-  var content2 = unboxColl(b2);
+  let content1 = unboxColl(b1);
+  let content2 = unboxColl(b2);
   if (content1.length !== collLength(b1)) {
 	  content1 = content1.slice(0, collLength(b1));
   }
-  var length2 = content2.length;
-  for (let i = 0; i < length2; i++) {
+  const length2 = collLength(b2);
+  for (let i = 0; i < length2; i=i+1) {
     content1.push(content2[i]);
   }
-  var result = boxColl(content1);
-  return result;
+  return boxColl(content1);
 }
 function minus(b1, b2) {
-  var result = [ ];
-  var v1 = unboxColl(b1).slice(0, collLength(b1));
-  var v2 = unboxColl(b2).slice(0, collLength(b2));
+  const result = [ ];
+  const v1 = unboxColl(b1).slice(0, collLength(b1));
+  const v2 = unboxColl(b2).slice(0, collLength(b2));
   v1.sort(compare);
   v2.sort(compare);
-  var i2=0;
-  var length2=v2.length;
-  var comp=0;
-  for (var i1=0; i1<v1.length; i1=i1+1) {
-    while ((i2 < length2) && (compare(v1[i1],v2[i2]) === 1)) i2=i2+1;
+  let i2 = 0;
+  const length2 = v2.length;
+  for (let i1 = 0; i1 < v1.length; i1=i1+1) {
+    while ((i2 < length2) && (compare(v1[i1],v2[i2]) === 1)) {
+      i2 = i2+1;
+    }
     if (i2 < length2) {
       if (compare(v1[i1],v2[i2]) === (-1)) { result.push(v1[i1]); } else { i2=i2+1; }
     } else {
@@ -324,16 +328,17 @@ function minus(b1, b2) {
   return boxColl(result);
 }
 function min(b1, b2) {
-  var result = [ ];
-  var v1 = unboxColl(b1).slice(0, collLength(b1));
-  var v2 = unboxColl(b2).slice(0, collLength(b2));
+  const result = [ ];
+  const v1 = unboxColl(b1).slice(0, collLength(b1));
+  const v2 = unboxColl(b2).slice(0, collLength(b2));
   v1.sort(compare);
   v2.sort(compare);
-  var i2=0;
-  var length2=v2.length;
-  var comp=0;
-  for (var i1=0; i1<v1.length; i1=i1+1) {
-    while ((i2 < length2) && (compare(v1[i1],v2[i2]) === 1)) i2=i2+1;
+  let i2 = 0;
+  const length2=v2.length;
+  for (let i1 = 0; i1 < v1.length; i1=i1+1) {
+    while ((i2 < length2) && (compare(v1[i1],v2[i2]) === 1)) {
+      i2=i2+1;
+    }
     if (i2 < length2) {
       if (compare(v1[i1],v2[i2]) === 0) result.push(v1[i1]);
     }
@@ -341,27 +346,28 @@ function min(b1, b2) {
   return boxColl(result);
 }
 function max(b1, b2) {
-  var result = [ ];
-  var v1 = unboxColl(b1).slice(0, collLength(b1));
-  var v2 = unboxColl(b2).slice(0, collLength(b2));
+  const result = [ ];
+  const v1 = unboxColl(b1).slice(0, collLength(b1));
+  const v2 = unboxColl(b2).slice(0, collLength(b2));
   v1.sort(compare);
   v2.sort(compare);
-  var i2=0;
-  var length2=v2.length;
-  var comp=0;
-  for (var i1=0; i1<v1.length; i1=i1+1) {
-    while ((i2 < length2) && (compare(v1[i1],v2[i2]) === 1)) { result.push(v2[i2]); i2=i2+1; }
+  let i2 = 0;
+  const length2 = v2.length;
+  for (let i1 = 0; i1 < v1.length; i1=i1+1) {
+    while ((i2 < length2) && (compare(v1[i1],v2[i2]) === 1)) {
+      result.push(v2[i2]); i2=i2+1;
+    }
     if (i2 < length2) {
       if (compare(v1[i1],v2[i2]) === 0) i2=i2+1;
     }
     result.push(v1[i1]);
   }
-  while (i2 < length2) { result.push(v2[i2]); i2=i2+1; }
+  while (i2 < length2) { result.push(v2[i2]); i2 = i2+1; }
   return boxColl(result);
 }
 function nth(b1, n) {
-  var index = n;
-  var content = unboxColl(b1);
+  const index = n;
+  const content = unboxColl(b1);
   if (0 <= index && index < collLength(b1)) {
     return boxLeft(content[index]);
   } else {
@@ -372,8 +378,8 @@ function count(v) {
   return arrayLength(v);
 }
 function contains(v, b) {
-  var content = unboxColl(b);
-  for (var i=0; i<collLength(b); i=i+1) {
+  const content = unboxColl(b);
+  for (let i = 0; i < collLength(b); i=i+1) {
     if (equal(v, content[i])) {
       return true;
     }
@@ -382,29 +388,32 @@ function contains(v, b) {
 }
 function compareOfMultipleCriterias(scl) {
   return function(a,b) {
-    var current_compare = 0;
-    for (var i=0; i<scl.length; i=i+1) {
-      var sc = scl[i];
-      if (Object.prototype.hasOwnProperty.call(sc,'asc')) { current_compare = compare(recDot(a,sc['asc']), recDot(b,sc['asc'])); }
-      else if (Object.prototype.hasOwnProperty.call(sc,'desc')) { current_compare = -(compare(recDot(a,sc['asc']), recDot(b,sc['asc']))); }
+    const current_compare = 0;
+    for (let i = 0; i < scl.length; i=i+1) {
+      const sc = scl[i];
+      if (Object.prototype.hasOwnProperty.call(sc,'asc')) {
+        current_compare = compare(recDot(a,sc['asc']), recDot(b,sc['asc']));
+      }
+      else if (Object.prototype.hasOwnProperty.call(sc,'desc')) {
+        current_compare = -(compare(recDot(a,sc['asc']), recDot(b,sc['asc'])));
+      }
 
       if (current_compare === -1) { return -1; }
       else if (current_compare === 1) { return 1; }
     }
     return current_compare;
   }
-  
 }
 function sort(b,scl) {
   if (scl.length === 0) { return b; } // Check for no sorting criteria
-  var compareFun = compareOfMultipleCriterias(scl);
+  const compareFun = compareOfMultipleCriterias(scl);
   /* Sorting in place leads to inconsistencies, notably as it re-orders the input WM in the middle of processing */
-  var result = unboxColl(b).slice(0, collLength(b));
+  const result = unboxColl(b).slice(0, collLength(b));
   result.sort(compareFun);
   return boxColl(result);
 }
 function groupByOfKey(l,k,keysf) {
-  var result = [ ];
+  const result = [ ];
   l.forEach((x) => {
     if (equal(keysf(x),k)) {
       result.push(x);
@@ -413,8 +422,8 @@ function groupByOfKey(l,k,keysf) {
   return boxColl(result);
 }
 function groupByNested(l,keysf) {
-  var keys = unboxColl(distinct(boxColl(l.map(keysf))));
-  var result = [ ];
+  const keys = unboxColl(distinct(boxColl(l.map(keysf))));
+  const result = [ ];
   keys.forEach((k) => {
     result.push({ 'keys': k, 'group' : groupByOfKey(l,k,keysf) });
   });
@@ -426,13 +435,13 @@ function groupBy(g,kl,l) {
   // g is partition name
   // kl is key list
   // l is input collection of records
-  var keysf = function (j) {
+  const keysf = function (j) {
     return recProject(j,kl);
   };
-  var grouped = groupByNested(l,keysf);
-  var result = [ ];
+  const grouped = groupByNested(l,keysf);
+  const result = [ ];
   grouped.forEach((x) => {
-    var gRec = {};
+    const gRec = {};
     gRec[g] = x.group;
     result.push(recConcat(x.keys, gRec));
   });
@@ -450,13 +459,13 @@ function substringEnd(v, start) {
   return v.substring(Number(start));
 }
 function stringJoin(sep, v) {
-  var content = unboxColl(v).slice(0, collLength(v));
+  const content = unboxColl(v).slice(0, collLength(v));
   return content.join(sep);
 }
 function like(pat, s) {
-  var reg1 = escapeRegExp(pat);
-  var reg2 = reg1.replace(/_/g, '.').replace(/%/g, '.*');
-  var reg3 = new RegExp(reg2);
+  const reg1 = escapeRegExp(pat);
+  const reg2 = reg1.replace(/_/g, '.').replace(/%/g, '.*');
+  const reg3 = new RegExp(reg2);
   return reg3.test(s);
 }
 
@@ -499,17 +508,17 @@ function natMaxPair(v1, v2) {
   return v2 < v1 ? v1 : v2;
 }
 function natSum(b) {
-  var content = unboxColl(b);
-  var result = 0n;
-  for (var i=0; i<collLength(b); i=i+1) {
+  const content = unboxColl(b);
+  let result = 0n;
+  for (let i = 0; i < collLength(b); i=i+1) {
     result += content[i];
   }
   return result;
 }
 function natMin(b) {
-  var content = unboxColl(b);
-  var result;
-  for (var i=0; i<collLength(b); i=i+1) {
+  const content = unboxColl(b);
+  let result = content[0] || 0;
+  for (let i = 0; i < collLength(b); i=i+1) {
     if (content[i] < result) {
       result = content[i];
     }
@@ -517,9 +526,9 @@ function natMin(b) {
   return result;
 }
 function natMax(b) {
-  var content = unboxColl(b);
-  var result;
-  for (var i=0; i<collLength(b); i=i+1) {
+  const content = unboxColl(b);
+  let result = content[0] || 0;
+  for (let i = 0; i < collLength(b); i=i+1) {
     if (content[i] > result) {
       result = content[i];
     }
@@ -527,7 +536,7 @@ function natMax(b) {
   return result;
 }
 function natArithMean(b) {
-  var len = collLength(b);
+  const len = collLength(b);
   if (len === 0) {
     return 0n;
   } else {
@@ -540,15 +549,15 @@ function floatOfNat(v) {
 
 /* Float */
 function floatSum(b) {
-  var content = unboxColl(b);
-  var result = 0;
-  for (var i=0; i<collLength(b); i=i+1) {
+  const content = unboxColl(b);
+  let result = 0;
+  for (let i = 0; i < collLength(b); i=i+1) {
     result += content[i];
   }
   return result;
 }
 function floatArithMean(b) {
-  var len = collLength(b);
+  const len = collLength(b);
   if (len === 0) {
     return 0;
   } else {
@@ -556,11 +565,11 @@ function floatArithMean(b) {
   }
 }
 function floatMin(b) {
-  var content = unboxColl(b).slice(0, collLength(b));
+  const content = unboxColl(b).slice(0, collLength(b));
   return Math.min.apply(Math,content);
 }
 function floatMax(b) {
-  var content = unboxColl(b).slice(0, collLength(b));
+  const content = unboxColl(b).slice(0, collLength(b));
   return Math.max.apply(Math,content);
 }
 function natOfFloat(v) {
--- runtimes/javascript/qcert-runtime-tostring.js
+++ runtimes/javascript/qcert-runtime-tostring.js
@@ -15,70 +15,71 @@
 /* JavaScript runtime for ToString operators */
 
 function toString(v) {
-    return toStringQ(v, '');
+  return toStringQ(v, '');
 }
 function toText(v) {
-    return toStringQ(v, '');
+  return toStringQ(v, '');
 }
 function toStringQ(v, quote) {
-    if (v === null) {
-        return 'null';
+  if (v === null) {
+    return 'null';
+  }
+  if (typeof v === 'object' && v.$coll && v.$length) {
+    v = unboxColl(v);
+  }
+  const t = typeof v;
+  if (t === 'string') {
+    return quote + v + quote;
+  }
+  if (t === 'boolean') {
+    return '' + v;
+  }
+  if (t === 'number') {
+    if (Math.floor(v) === v) { return (new Number(v)).toFixed(1); } // Make sure there is always decimal point
+    else { return '' + v; }
+  }
+  if (t === 'bigint') {
+    return '' + v;
+  }
+  if ({}.toString.apply(v) === '[object Array]') {
+    v = v.slice();
+    v.sort();
+    let result = '[';
+    const n = v.length;
+    for (let i = 0; i < n; i=i+1) {
+      if (i > 0) {
+        result += ', ';
+      }
+      result += toStringQ(v[i], quote);
     }
-    if (typeof v === 'object' && v.$coll && v.$length) {
-        v = unboxColl(v);
+    return result + ']';
+  }
+  let result2 = '';
+  if (v.$class) { // branded value
+    let cl = v.$class;
+    if (typeof cl === 'object' && cl.$coll && cl.$length) {
+      cl = unboxColl(cl);
     }
-    var t = typeof v;
-    if (t === 'string') {
-        return quote + v + quote;
+    result2 += '<';
+    result2 += cl;
+    result2 += ':';
+    result2 += toStringQ(v.$data, quote);
+    result2 += '>';
+  } else { // record
+    // First need to sort
+    const sortable = [];
+    for (const key in v) {
+      sortable.push({ key: key, val: v[key] });
     }
-    if (t === 'boolean') {
-        return '' + v;
+    sortable.sort(function(a, b) { return a.key.localeCompare(b.key); });
+    result2 += '{';
+    let first = true;
+    const n = sortable.length;
+    for (let i = 0; i < n; i=i+1) {
+      if (first) { first = false; } else { result2 += ', '; }
+      result2 += toStringQ(sortable[i].key, quote) + '->' + toStringQ(sortable[i].val, quote);
     }
-    if (t === 'number') {
-        if (Math.floor(v) === v) { return (new Number(v)).toFixed(1); } // Make sure there is always decimal point
-        else { return '' + v; }
-    }
-    if (t === 'bigint') {
-        return '' + v;
-    }
-    if ({}.toString.apply(v) === '[object Array]') {
-        v = v.slice();
-        v.sort();
-        var result = '[';
-        for (var i=0, n=v.length; i<n; i=i+1) {
-            if (i > 0) {
-                result += ', ';
-            }
-            result += toStringQ(v[i], quote);
-        }
-        return result + ']';
-    }
-    var result2 = '';
-    if (v.$class) { // branded value
-        let cl = v.$class;
-        if (typeof cl === 'object' && cl.$coll && cl.$length) {
-            cl = unboxColl(cl);
-        }
-        result2 += '<';
-        result2 += cl;
-        result2 += ':';
-        result2 += toStringQ(v.$data, quote);
-        result2 += '>';
-    } else { // record
-        // First need to sort
-        var sortable = [];
-        for (var key in v) {
-            sortable.push({ key: key, val: v[key] });
-        }
-        sortable.sort(function(a, b) { return a.key.localeCompare(b.key); });
-        var result2 = '{';
-        var first = true;
-        for (var i=0, n=sortable.length; i<n; i=i+1) {
-            if (first) { first = false; } else { result2 += ', '; }
-            result2 += toStringQ(sortable[i].key, quote) + '->' + toStringQ(sortable[i].val, quote);
-        }
-        result2 += '}';
-    }
-    return result2 + '';
+    result2 += '}';
+  }
+  return result2 + '';
 }
-
